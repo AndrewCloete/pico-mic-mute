83822fdd288f91a3e24cf5b2025b2d77d06a20c5
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2023-11-30 06:09:27 +0200

    Adds latch logic with indicator LED

--- examples/usb_microphone/main.c
+++ examples/usb_microphone/main.c
@@ -2,20 +2,25 @@
  * Copyright (c) 2021 Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
- * 
+ *
  * This examples creates a USB Microphone device using the TinyUSB
  * library and captures data from a PDM microphone using a sample
  * rate of 16 kHz, to be sent the to PC.
- * 
+ *
  * The USB microphone code is based on the TinyUSB audio_test example.
- * 
+ *
  * https://github.com/hathach/tinyusb/tree/master/examples/device/audio_test
  */
 
-//#include "pico/pdm_microphone.h"
+#include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 #include "pico/analog_microphone.h"
 #include "usb_microphone.h"
+
+#define PTT_PIN 14
+#define LATCH_PIN 15
+#define LED_PIN PICO_DEFAULT_LED_PIN
 
 // configuration
 const struct analog_microphone_config config = {
@@ -34,6 +39,10 @@ const struct analog_microphone_config config = {
 
 // variables
 uint16_t sample_buffer[SAMPLE_BUFFER_SIZE];
+uint16_t mute_buffer[SAMPLE_BUFFER_SIZE];
+bool latch_state = false;
+bool pin_state = false;
+bool pin_ack = false;
 
 // callback functions
 void on_analog_samples_ready();
@@ -41,6 +50,13 @@ void on_usb_microphone_tx_ready();
 
 int main(void)
 {
+  stdio_init_all();
+  gpio_init(LATCH_PIN);
+  gpio_set_dir(LATCH_PIN, GPIO_IN);
+  gpio_pull_up(LATCH_PIN);
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
   // initialize and start the Analog microphone
   analog_microphone_init(&config);
   analog_microphone_set_samples_ready_handler(on_analog_samples_ready);
@@ -50,9 +66,32 @@ int main(void)
   usb_microphone_init();
   usb_microphone_set_tx_ready_handler(on_usb_microphone_tx_ready);
 
-  while (1) {
+  while (1)
+  {
     // run the USB microphone task continuously
     usb_microphone_task();
+    pin_state = gpio_get(LATCH_PIN);
+    if (pin_state)
+    {
+      pin_ack = false;
+    }
+    if (!pin_state && !pin_ack)
+    {
+      sleep_ms(50); // debounce
+      if (!gpio_get(LATCH_PIN))
+      {
+        latch_state = !latch_state;
+        if (latch_state)
+        {
+          gpio_put(LED_PIN, 1);
+        }
+        else
+        {
+          gpio_put(LED_PIN, 0);
+        }
+        pin_ack = true;
+      }
+    }
   }
 
   return 0;
@@ -61,7 +100,7 @@ int main(void)
 void on_analog_samples_ready()
 {
   // callback from library when all the samples in the library
-  // internal sample buffer are ready for reading 
+  // internal sample buffer are ready for reading
   analog_microphone_read(sample_buffer, SAMPLE_BUFFER_SIZE);
 }
 
@@ -71,5 +110,12 @@ void on_usb_microphone_tx_ready()
   // to be transmitted.
   //
   // Write local buffer to the USB microphone
-  usb_microphone_write(sample_buffer, sizeof(sample_buffer));
+  if (latch_state)
+  {
+    usb_microphone_write(sample_buffer, sizeof(sample_buffer));
+  }
+  else
+  {
+    usb_microphone_write(mute_buffer, sizeof(mute_buffer));
+  }
 }
